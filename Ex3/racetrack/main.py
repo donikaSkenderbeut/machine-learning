8406320a0717dbfb20d1bce1c549112a24ceb7a2
DonikaSkenderbeut <e11929150@student.tuwien.ac.at>
2023-01-20 16:20:25 +0100

    small changes in names

--- Ex3/racetrack/main.py
+++ Ex3/racetrack/main.py
@@ -13,7 +13,7 @@ fin_cells = {(26, cols-1), (27, cols-1), (28, cols-1), (29, cols-1), (30, cols-1
 epsilon = 0.1
 gamma = 1
 
-# action can be horizontal and vertical with value 0, 1 or -1
+# actions: velocity increments at a given time, 9 actions 0, 1, -1
 actions = [
     (0, 0),
     (0, 1),
@@ -29,17 +29,6 @@ act_len = len(actions)
 
 # 5 velocity values 0 to 4
 vel_len = 5
-
-# initialise Q, C and π
-# state is 4-tuple: (row, col, velocity_horizontal, velocity_vertical)
-Q = np.random.rand(rows, cols, vel_len, vel_len, act_len) * 400 - 500
-C = np.zeros((rows, cols, vel_len, vel_len, act_len))
-pi = np.ones((rows, cols, vel_len, vel_len), dtype=int)
-for r in range(rows):
-    for c in range(cols):
-        for h in range(vel_len):
-            for v in range(vel_len):
-                pi[r, c, h, v] = np.argmax(Q[r, c, h, v])
 
 # compute all valid actions for each velocity combination i.e vertical 0 horizontal 0, v 1 h 0 ...
 # Both velocity components are restricted to be non-negative and less than 5, and they cannot both be zero.
@@ -58,7 +47,21 @@ for h in range(vel_len):
         dict_valid_acts[(h, v)] = valid_acts[j]
         j += 1
 
-# initialize state, action, behaviour policy arrays
+# initialise Q, C and pi
+# Q is the value matrix given state and action
+# C is a cumulative sum of the weights
+# pi is the target policy
+# state is a tuple made of 4 components (x-coordinate, y-coordinate, vertical velocity, horizontal velocity)
+Q = np.random.rand(rows, cols, vel_len, vel_len, act_len) * -400  # broader range of numbers makes computation faster
+C = np.zeros((rows, cols, vel_len, vel_len, act_len))
+pi = np.ones((rows, cols, vel_len, vel_len), dtype=int)
+for r in range(rows):
+    for c in range(cols):
+        for h in range(vel_len):
+            for v in range(vel_len):
+                pi[r, c, h, v] = np.argmax(Q[r, c, h, v])
+
+# initialize state, action, behaviour policy probability arrays
 tmp = np.empty((), dtype=object)
 tmp[()] = (0, 0, 0, 0)
 S = np.full(10**6, tmp, dtype=object)
@@ -66,22 +69,22 @@ A = np.empty((10**6), dtype=int)
 B = np.empty((10**6), dtype=float)
 
 
-def make_trajectory(eps, noise=True):
+def learn_policy(eps, noise=True):
     t = 0
 
-    # start state
+    # start state (0, random start column, 0 v-velocity, 0 h-velocity)
     S[t] = (0, np.random.choice(start_cols), 0, 0)
 
     while True:
         s = S[t]
-        # get all valid actions for current velocity
+        # get indices of all valid actions for current velocity
         acts = dict_valid_acts[(s[2], s[3])]
         num_acts = len(acts)
 
-        # choose next action using ε-greedy policy if πa is valid
+        # choose next action and generate behavioral policy using ε-greedy policy if pi(s)=a is valid
         # otherwise choose randomly, and save its probability
         pi_a = pi[s[0], s[1], s[2], s[3]]
-        pi_a_valid = pi_a in acts
+        pi_a_valid = pi_a in acts # if pi(s) returns valid index that can be found in the valid action indices then True
         if np.random.rand() >= eps:
             if pi_a_valid:
                 a = pi_a
@@ -105,32 +108,30 @@ def make_trajectory(eps, noise=True):
         act = actions[a]
 
         # next state
-        vel = (s[2] + act[0], s[3] + act[1])
-        next_s = (s[0] + vel[1], s[1] + vel[0], vel[0], vel[1])
+        new_vel = (s[2] + act[0], s[3] + act[1])
+        next_s = (s[0] + new_vel[1], s[1] + new_vel[0], new_vel[0], new_vel[1])
 
         # check if car hits finish line by computing "path", which is something between s and next_s, thus checking if
         # any of the driven paths from s to next_s crossed the finish line
         path = set([
             (min(s[0] + i, next_s[0]), min(s[1] + i, next_s[1]))
-            for i in range(max(vel[0], vel[1]))
+            for i in range(max(new_vel[0], new_vel[1]))
         ])
         if len(path.intersection(fin_cells)) > 0:
             return t
 
         # check if car hits boundary
         if not (0 <= next_s[0] < rows) or not (0 <= next_s[1] < cols) or racetrack[rows-1-next_s[0]][next_s[1]] == 'X':
-            # if car hits boundary, go back to start
+            # go back to start
             s = (0, np.random.choice(start_cols), 0, 0)
         else:
-            # otherwise go to the next state
             s = next_s
 
-        # append state to trajectory
         t += 1
         S[t] = s
 
 
-def run_episode(T):
+def apply_mc_control(T):
     G = 0.0
     W = 1.0
     R = -1
@@ -152,10 +153,10 @@ def run_episode(T):
 
 
 # print several trajectories following the optimal policy
-def output_trajectories():
-    for i in range(4):
-        T = make_trajectory(0.0, noise=False)
-        print("\noptimal trajectory #{}:".format(i))
+def optimal_trajectories():
+    for i in range(3):
+        T = learn_policy(0.0, noise=False)
+        print("\noptimal trajectory #{}:".format(i+1))
         print("S: ", S[0:T+1])
         print("A: ", A[0:T+1])
         print("R: ", -1 * T)
@@ -165,14 +166,11 @@ episode_num = 10 ** 5
 rewards = []
 
 for i in range(0, episode_num + 1):
-    T = make_trajectory(epsilon)
-    t = run_episode(T)
-    print("episode {}: {}, {}, {}".format(i, T, t, T - t))
-
-    if i % 9 == 0:
-        T = make_trajectory(0.0)
-        rewards.append(-1 * T)
+    T = learn_policy(epsilon)
+    t = apply_mc_control(T)
+    print("Episode {}: {}, {}".format(i, T, t,))
+    rewards.append(-1 * T)
 
 plt.plot(rewards)
-output_trajectories()
+optimal_trajectories()
 plt.show()
