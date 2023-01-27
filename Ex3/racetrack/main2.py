7fdaab31d4fcace06a70f7c348b5a25369876b41
lburtscher <lukas.burtscher@check24.at>
2023-01-27 15:28:01 +0100

    main 2

--- Ex3/racetrack/main2.py
+++ Ex3/racetrack/main2.py
@@ -1,6 +1,8 @@
 from generator import RacetrackGenerator
 from environment import Environment
 from montecarlo import MonteCarlo
+from agent import Agent
+import matplotlib.pyplot as plt
 
 gen = RacetrackGenerator()
 racetrack = gen.racetrack1_from_book()
@@ -8,4 +10,24 @@ epsilon = 0.1
 gamma = 1
 velocity_constraint = 5
 env = Environment(gen, 5)
-agent =
+mc = MonteCarlo(env,gen,velocity_constraint,gamma)
+agent = Agent(epsilon,gen,env,mc)
+
+episodes = 10 ** 5
+rewards = []
+
+for i in range(episodes+1):
+    T = agent.learn_policy()
+    S,A,B = agent.get_sab()
+    t = mc.apply_mc_control(S, T, A, B)
+    print("Episode {}: T={}, t={}, R={}".format(i, T, t, -1 * T))
+    rewards.append(-1 * T)
+
+k10_rewards = [rewards[i] for i in range(0, episodes + 1, 10000)]
+plt.plot(k10_rewards)
+plt.xlabel("Episodes (in ten thousands)")
+plt.ylabel("Rewards")
+
+# change based on which racetrack we are choosing
+plt.title("Rewards from Racetrack 1")
+plt.show()
--- Ex3/racetrack/montecarlo.py
+++ Ex3/racetrack/montecarlo.py
@@ -3,7 +3,7 @@ from generator import RacetrackGenerator
 import numpy as np
 
 class MonteCarlo:
-    def __init__(self, env:Environment, racetrack:RacetrackGenerator,velocity_constraint: int, gamma:int):
+    def __init__(self, env:Environment, generator:RacetrackGenerator, velocity_constraint: int, gamma:int):
         # initialise Q, C and pi
         # Q is the value matrix given state and action
         # C is a cumulative sum of the weights
@@ -11,8 +11,8 @@ class MonteCarlo:
         # state is a tuple made of 4 components (x-coordinate, y-coordinate, vertical velocity, horizontal velocity)
         vel_len = velocity_constraint
         act_len = len(env.get_all_actions())
-        rows = racetrack.get_dimensions()[0]
-        cols = racetrack.get_dimensions()[1]
+        rows = generator.get_dimensions()[0]
+        cols = generator.get_dimensions()[1]
         self.env = env
         self.gamma = gamma
         self.Q = np.random.rand(rows, cols, vel_len, vel_len, act_len) * -400  # broader range of numbers makes computation faster
@@ -24,7 +24,7 @@ class MonteCarlo:
                     for v in range(vel_len):
                         self.pi[r, c, h, v] = np.argmax(self.Q[r, c, h, v])
 
-    def apply_mc_control(self, state, S, T, A, B):
+    def apply_mc_control(self, S, T, A, B):
         G = 0.0
         W = 1.0
         R = -1
@@ -48,3 +48,7 @@ class MonteCarlo:
 
             W /= B[t]
         return 0
+
+
+    def get_action_from_target_policy(self, state):
+        return self.pi[state[0], state[1], state[2], state[3]]
